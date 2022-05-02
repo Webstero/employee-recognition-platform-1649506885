410402a28acea88efd3fdbc832ae0cdebe9e2136
Webstero <kacper.lepecki@gmail.com>
2022-05-02 20:08:08 +0200

    Update tests of Admin and Employee models.

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
+  let(:admin) { create(:admin) }
+
   context 'when validation is successful' do
     it 'returns true' do
       expect(build(:admin)).to be_valid
@@ -14,6 +16,10 @@ RSpec.describe Admin, type: :model do
 
     it 'is not valid without a password' do
       expect(build(:admin, password: '')).not_to be_valid
+    end
+
+    it 'is not valid with the same email' do
+      expect(build(:admin, email: admin.email)).not_to be_valid
     end
   end
 end
