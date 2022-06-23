4521b193dbfce9ed8b03f2621d15c046544b2c30
Webstero <kacper.lepecki@gmail.com>
2022-06-23 14:35:50 +0200

    Change about Code Review.

---
+++ app/services/orders/save_service.rb
@@ -0,0 +1,28 @@
+module Orders
+  class EarnedPointsException < StandardError; end
+
+  class SaveService
+    def call(order:, employee:)
+      price = order.reward.price
+      validate_earned_points(employee: employee, price: price)
+      ActiveRecord::Base.transaction do
+        order.price = price
+        order.save!
+        pay_for_order(employee: employee, price: price)
+      end
+    end
+
+    private
+
+    def validate_earned_points(employee:, price:)
+      return unless price > employee.earned_points
+
+      raise ::Orders::EarnedPointsException,
+            'You dont`t have enough erned points to buy a rewards.'
+    end
+
+    def pay_for_order(employee:, price:)
+      employee.update!(earned_points: employee.earned_points - price)
+    end
+  end
+end
--- spec/features/order_process/constant_price_spec.rb
+++ spec/features/order_process/constant_price_spec.rb
@@ -4,6 +4,7 @@ require 'capybara/rspec'
 RSpec.describe 'Employee checking constant price of bougth rewards', type: :feature do
   let!(:reward) { create(:reward, price: 1) }
   let!(:employee) { create(:employee, earned_points: 1) }
+  let!(:admin) { create(:admin) }
   let!(:order) { create(:order, reward_id: reward.id, employee_id: employee.id, price: reward.price) }
 
   before do
@@ -12,9 +13,30 @@ RSpec.describe 'Employee checking constant price of bougth rewards', type: :feat
   end
 
   it 'Success' do
-    expect(page).to have_text order.price
-    reward.update!(price: 2.0)
-    visit orders_path
-    expect(page).to have_text order.price
+    expect(page).to have_text '1.0'
+
+    click_link 'Sign out'
+
+    visit new_admin_session_path
+    fill_in 'Email', with: admin.email
+    fill_in 'Password', with: admin.password
+    click_button 'Log in'
+
+    click_link 'Manage Rewards'
+    all('a', text: 'Edit')[0].click
+    fill_in 'Price', with: '2.0'
+    click_button 'Update Reward'
+    expect(page).to have_content 'Reward was successfully updated.'
+    expect(page).to have_current_path(admins_rewards_path)
+
+    click_link 'Sign out'
+
+    visit new_employee_session_path
+    fill_in 'Email', with: employee.email
+    fill_in 'Password', with: employee.password
+    click_button 'Log in'
+
+    click_link 'My orders'
+    expect(page).to have_text '1.0'
   end
 end
