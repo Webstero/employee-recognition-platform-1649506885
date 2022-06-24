5941786bb0db0a6f1631614181ab5ebadacd48e5
Webstero <kacper.lepecki@gmail.com>
2022-06-24 14:46:44 +0200

    Update tests due added serialize bought reward in model Order.

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :order do
     reward
     employee
-    price { reward.price }
+    bought_reward { reward }
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,7 +4,5 @@ RSpec.describe Order, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:reward) }
     it { is_expected.to belong_to(:employee) }
-    it { is_expected.to validate_presence_of(:price) }
-    it { is_expected.not_to allow_value(0).for(:price) }
   end
 end
