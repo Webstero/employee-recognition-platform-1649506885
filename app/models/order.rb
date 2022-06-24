4009bc034febe069bd76c4ce4b356769ba5aac65
Webstero <kacper.lepecki@gmail.com>
2022-06-24 14:38:40 +0200

    Add serialize method to store bought reward. Update views, controller and save_service of order.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :employee
   belongs_to :reward
-  validates :price, numericality: { greater_than_or_equal_to: 1 }, presence: true
+
+  serialize :bought_reward
 end
